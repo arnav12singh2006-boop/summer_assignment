a88fbd5dd875b0029d78c0aa243c3a92ff43457a
Arnav singh <arnav.12.singh.2006@gmail.com>
2026-06-08 22:02:07 +0530

    Add files via upload

---
+++ day8/number_triangle.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int count=0;
+    for(int i= 0; i<=5; i++){
+        
+        for(int j =1;  j <=i; j++)
+        
+        cout <<  j << " " ;
+        cout << endl;
+        
+    }
+   
+
+    return 0;
+}
