093d1f380577fc2610ae04a9c1b5e3e4e8de6601
Arnav singh <arnav.12.singh.2006@gmail.com>
2026-06-09 22:48:11 +0530

    Add files via upload

---
+++ day10/star_pyramid_2by2.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int main() 
+{
+    int n = 5;
+
+    for(int row = 1; row <= n; row++) {
+
+        // Print spaces
+        for(int space = 1; space <= n - row; space++) {
+            cout << " ";
+        }
+
+        // Print stars
+        for(int star = 1; star <= 2 * row - 1; star++) {
+            cout << "*";
+        }
+
+        cout << endl;
+    }
+
+    return 0;
+}
